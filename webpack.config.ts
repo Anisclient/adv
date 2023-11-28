0018f2d3ce2a6a0e15eeb9a2f4bd324723631b0f
Anis Almukhametov <anisclient@gmail.com>
2023-11-28 17:18:42 +0300

    separate dev and prod build

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,24 +1,26 @@
 import path from 'path'
 import webpack from 'webpack'
 import { buildWebpackConfig } from './config/build/buildWebpackConfig'
-import { BuildMode, BuildPaths } from './config/build/types/config'
+import { BuildEnv, BuildMode, BuildPaths } from './config/build/types/config'
 
-const mode: BuildMode = 'development'
-const isDev = mode === 'development'
+export default (env: BuildEnv) => {
+  const mode: BuildMode = env.mode || 'development'
+  const PORT = env.port || 3000
 
-const PORT = 3000
+  const isDev = mode === 'development'
 
-const paths: BuildPaths = {
-  build: path.resolve(__dirname, 'build'),
-  entry: path.resolve(__dirname, 'src', 'index.ts'),
-  html: path.resolve(__dirname, 'public', 'index.html'),
+  const paths: BuildPaths = {
+    build: path.resolve(__dirname, 'build'),
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    html: path.resolve(__dirname, 'public', 'index.html'),
+  }
+
+  const config: webpack.Configuration = buildWebpackConfig({
+    mode,
+    paths,
+    isDev,
+    port: PORT,
+  })
+
+  return config
 }
-
-const config: webpack.Configuration = buildWebpackConfig({
-  mode,
-  paths,
-  isDev,
-  port: PORT,
-})
-
-export default config
