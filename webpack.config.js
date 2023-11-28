7a064dc5563aa1b23df881709ced1f30f2e4e816
Anis Almukhametov <anisclient@gmail.com>
2023-11-28 14:29:06 +0300

    add typescript and ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HTMLWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'production', // development or production mode
-  entry: path.resolve(__dirname, 'src', 'index.js'), // entry point
+  entry: path.resolve(__dirname, 'src', 'index.ts'), // entry point
   output: {
     filename: '[name].[contenthash].js', // name - 'main' by default, contenthash - unique hash based on the content
     path: path.resolve(__dirname, 'build'), // build dir
@@ -16,4 +16,16 @@ module.exports = {
     }),
     new webpack.ProgressPlugin(), // show build progress
   ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader', // use ts-loader to load .ts and .tsx files
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'], // import a from '/.a' instead of import a from './a.tsx'
+  },
 }
