07e70ef56e7118f6d2e00700bcbdb196a92e9dc6
Anis Almukhametov <anisclient@gmail.com>
2023-11-28 21:19:54 +0300

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import { Counter } from './components/Counter'
+import { Suspense } from 'react'
+import { Route, Routes, Link } from 'react-router-dom'
 import './index.scss'
+import AboutPage from './pages/AboutPage'
+import MainPage from './pages/MainPage'
 
 const App = () => {
   return (
     <div className="app">
-      <Counter />
+      <Link to={'/'}>Main</Link>
+      <Link to={'/about'}>About</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={'/about'} element={<AboutPage />} />
+          <Route path={'/'} element={<MainPage />} />
+        </Routes>
+      </Suspense>
     </div>
   )
 }
