f91c4fdcf9e4c3fb487f29181cedd3c0ef2a4fb7
Anis Almukhametov <anisclient@gmail.com>
2023-11-30 20:01:22 +0300

    change Sidebar collapse button

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,9 @@ import cls from './Sidebar.module.scss'
 import { classNames } from 'shared/lib/classNames'
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher'
 import { LangSwitcher } from 'widgets/LangSwitcher'
+import { Button, ThemeButton } from 'shared/ui/Button/Button'
+import ArrowLeft from '../../../../shared/assets/icons/arrow-left.svg'
+import ArrowRight from '../../../../shared/assets/icons/arrow-right.svg'
 
 interface SidebarProps {
   className?: string
@@ -11,11 +14,13 @@ interface SidebarProps {
 export const Sidebar = ({ className }: SidebarProps) => {
   const [collapsed, setCollapset] = useState(false)
 
-  const toggle = () => setCollapset((prev) => !prev)
+  const collapse = () => setCollapset((prev) => !prev)
 
   return (
     <div className={classNames(cls.sidebar, { [cls.collapsed]: collapsed }, [className])}>
-      <button onClick={toggle}>Toggle</button>
+      <Button theme={ThemeButton.CLEAR} onClick={collapse} className={cls.sidebar__button}>
+        {collapsed ? <ArrowRight /> : <ArrowLeft />}
+      </Button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
         <LangSwitcher className={cls.sidebar__lang} />
