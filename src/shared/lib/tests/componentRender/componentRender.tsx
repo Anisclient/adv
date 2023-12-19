d287d9f8eb02ef3748fd23d47194bb6b7350f80a
Anis Almukhametov <anisclient@gmail.com>
2023-12-19 23:56:45 +0300

    fix tests

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -15,10 +15,10 @@ export function componentRender(component: ReactNode, options: ComponentRenderOp
   const { route = '/', initialState } = options;
 
   return render(
-    <StoreProvider initialState={initialState}>
-      <MemoryRouter initialEntries={[route]}>
+    <MemoryRouter initialEntries={[route]}>
+      <StoreProvider initialState={initialState}>
         <I18nextProvider i18n={i18nTests}>{component}</I18nextProvider>
-      </MemoryRouter>
-    </StoreProvider>,
+      </StoreProvider>
+    </MemoryRouter>,
   );
 }
